fd653d2c747acbebcbcfd9cd03e1be6011e0108b
zxcbezdar <maksikplayb@gmail.com>
2023-04-22 11:01:54 +0300

    commit

---
+++ types/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("тип данных byte в диапазоне от 0 до 255 размер 1 byte");
+Console.WriteLine("тип данных sbyte в диапазоне от -128 до 127 размер 1 byte");
+Console.WriteLine("тип данных short в диапазоне от -32768 до 32767 размер 2 bytes");
+Console.WriteLine("тип данных ushort в диапазоне от 0 до 65535 размер 2 bytes");
+Console.WriteLine("тип данных int в диапазоне от -2147483648 до 2147483647 размер 4 bytes");
+Console.WriteLine("тип данных uint в диапазоне от 0 до 4294967295 размер 4 bytes");
+Console.WriteLine("тип данных long в диапазоне от -9223372036854775808 до 9223372036854775807 размер 8 bytes");
+Console.WriteLine("тип данных ulong в диапазоне от 0 до 18446744073709551615 размер 8 bytes");
+Console.WriteLine("тип данных float в диапазоне от -3.402823e38 до 3.402823e38 размер 4 bytes");
+Console.WriteLine("тип данных double в диапазоне от -1.79769313486232e308 до 1.79769313486232e308 размер 8 bytes");
+Console.WriteLine("тип данных decimal в диапазоне от -79228162514264337593543950335 до 79228162514264337593543950335 размер 16 bytes");
+Console.WriteLine("тип данных char в диапазоне от U+0000 до U+FFFF размер 2 bytes");
+Console.WriteLine("тип данных bool в диапазоне от false до true размер 1 byte");
